5b12facd686447d98822c236fb95ebd209c6b8ff
maicarem <you@example.com>
2023-03-16 20:44:18 +0100

    Change parameter, fix, minor issues

--- src/dat.jl
+++ src/dat.jl
@@ -17,7 +17,7 @@ ABSOLUTE_OPTIMALITY_GAP = 1e-6
 
 # Declare sets
 V = 1:n
-V_tilt = [1,2,3,4]
+V_tilt = [1,2,3,4,5,6]
 V_certain = [i for i in V if i ∉ V_tilt]
 A = [(i,j) for i in V for j in V if i !=j]
 A_prime = [(i,j) for i in V for j in V]
@@ -28,7 +28,7 @@ T_tilt = [(i,j) for i in V_tilt for j in V_tilt if i<j]
 
 # Instance transformation
 function _find_offset_star()
-    offset = Dict{Int, Float64}()
+    offset = zeros(Float64, n)
     for i in 1:n
         if length(V_tilt) <= 1
             offset[i] = minimum(3 * sc[i,[j for j in 1:n if i!=j && j in V_certain]])
@@ -42,7 +42,7 @@ function _find_offset_star()
 end
 
 function _find_offset_backup()
-    offset_backup = Dict{Int, Float64}()
+    offset_backup = zeros(Float64, n)
     for i in 1:n
         offset_backup[i] = minimum(rc[1:n .!= i, i])
     end
@@ -51,17 +51,17 @@ end
 
 function _transformation_cost()
     
-    star_offset_dict = _find_offset_star()
+    star_offset_list = _find_offset_star()
     backup_offset_dict = _find_offset_backup()
-    offset = sum([star_offset_dict[i] for i in 1:n])
+    offset = sum([star_offset_list[i] for i in 1:n])
     
-    opening_cost = Dict{Int, Float64}()
+    opening_cost = zeros(Float64, n)
     ring_cost = ones(Float64, n,n) * 1e18
     star_cost = ones(Float64, n,n) * 1e18
     backup_cost = ones(Float64, n,n) * 1e18
 
     for i in 1:n
-        opening_cost[i] = oc[i] - star_offset_dict[i]
+        opening_cost[i] = oc[i] - star_offset_list[i]
         for j in 1:n
             i<j || continue
             if i in V_tilt && j in V_certain
@@ -75,20 +75,22 @@ function _transformation_cost()
             end
             
             backup_cost[i,j] = rc[i,j] - 1/2 * backup_offset_dict[i] - 1/2 * backup_offset_dict[j]
-            backup_cost[j,i] = backup_cost[i,j]
+            
             if j in V_certain
                 j!=i || continue
-                star_cost[i,j] = sc[i,j] - star_offset_dict[i]
+                star_cost[i,j] = sc[i,j] - star_offset_list[i]
             else
                 j!=i || continue
-                star_cost[i,j] = sc[i,j] - 1/2 * star_offset_dict[i]
+                star_cost[i,j] = sc[i,j] - 1/3 * star_offset_list[i]
             end
+            
             ring_cost[j,i] = ring_cost[i,j]
             star_cost[j,i] = star_cost[i,j]
+            backup_cost[j,i] = backup_cost[i,j]
         end
     end
     return offset, opening_cost, ring_cost, star_cost, backup_cost
 end
-
+opening_cost, ring_cost, star_cost = oc, rc, sc
 offset, oc, rc, sc, backup = _transformation_cost()
-@show backup
+@show opening_cost
