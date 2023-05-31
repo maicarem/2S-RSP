0c1eb9078a50520d20f0eb0d618bff119581e027
maicarem <you@example.com>
2023-05-31 09:22:37 +0200

    Collect data ILP

--- src/rsp_ilp.jl
+++ src/rsp_ilp.jl
@@ -5,9 +5,29 @@ using Graphs, GraphsFlows
 include("misc.jl")
 include("user_cut.jl")
 
-starting_time = time()
-
+result_dict = Dict()
 main = Model(optimizer_with_attributes(Gurobi.Optimizer))
+
+for param_name in ["num_constraint_ilp_including_integrality", 
+                    "num_constraint_ilp_notinclude_integrality", 
+                    "lower_bound", 
+                    "upper_bound", 
+                    "num_subtour", 
+                    "num_hubs",
+                    "time_sp",
+                    "time_master",
+                    "total_time",
+                    "num_cut_sp0",
+                    "num_cut_spi",
+                    "obj_bf3",
+                    "obj_bf4",
+                    "obj_bf5",
+                    "time_bf3",
+                    "time_bf4",
+                    "time_bf5",
+                    "timestamp"]
+    result_dict[param_name] = 0
+end
 
 # Decision variables
 @variable(main, x[i in V, j in V; i<j], Bin)
@@ -43,24 +63,55 @@ end
 @constraint(main, [(i,j) in A], y[i,j] <= y[j,j])
 @constraint(main, y[1,1] == 1)
 
+function my_callback_subtour(cb_data)
+    
+    x_hat = Bool.(round.(callback_value.(cb_data, x)))
+    y_hat1 = Bool.(round.(callback_value.(cb_data, y)))
+    x_hat = _transform_matrix(x_hat)
+    
+    status = callback_node_status(cb_data, main)
+    
+    y_hat = zeros(Bool, n)
+    for i in 1:n
+        y_hat1[i,i] == 1 || continue
+        y_hat[i] = 1
+    end
+    
+    all_cycles = find_cycle(x_hat, y_hat)
+
+    if status == MOI.CALLBACK_NODE_STATUS_INTEGER
+        # Check subtour in a tour
+        if length(all_cycles) > 1
+            _list_hub = [i for i in 1:n if y_hat[i] == 1]
+            # add subtour elimination
+            for each_cycle in all_cycles
+                con = @build_constraint(length(each_cycle) - 1/(length(_list_hub)- length(each_cycle))*sum(y[i,i] for i in _list_hub if i ∉ each_cycle)>= 
+                sum(x[minmax(each_cycle[i], each_cycle[i+1])] for i in eachindex(each_cycle[1:end-1]))+ x[minmax(each_cycle[1], each_cycle[end])])
+                MOI.submit(main, MOI.LazyConstraint(cb_data), con)
+                result_dict["num_subtour"] += 1
+            end
+        end
+    end
+end
+
+
 set_attribute(main, MOI.LazyConstraintCallback(), my_callback_subtour)
 if pars.uc
     set_attribute(main, MOI.UserCutCallback(), call_back_user_cuts)
 end
 
 optimize!(main)
-@show value.(x_prime)
-# @show value.(x_prime_prime)
-@show value.(x)
+
+result_dict["num_constraint_ilp_including_integrality"] = num_constraints(main; count_variable_in_set_constraints = true)
+result_dict["num_constraint_ilp_notinclude_integrality"] = num_constraints(main; count_variable_in_set_constraints = false)
+result_dict["lower_bound"] = objective_bound(main)
+result_dict["upper_bound"] = objective_value(main)
+result_dict["num_hubs"] = sum(value(y[i,i]) for i in 1:n)
+result_dict["total_time"] = solve_time(main)
+result_dict["timestamp"] = now()
+
+println(result_dict)
 
 
-println("========================")
-for i in 1:n
-    for j in 1:n
-        println("rc[$(i), $(j)] = $(rc[i,j])")
-        println("ring_cost[$(i), $(j)] = $(ring_cost[i,j])")
-        println("backup[$(i), $(j)] = $(backup[i,j])")
-    end
-end
 
-_write_gurobi_log(main, "ilp", name, pars, n, V_tilt, 0)
+# _write_gurobi_log(main, "ilp", name, pars, n, V_tilt, 0)
